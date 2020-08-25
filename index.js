453c36a10c2780272e212e56b3dd83508301089e
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2020-08-25 10:42:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+
+
+function upperCaseCustomerName() {
+
+    customerName = customerName.toUpperCase()
+
+    return customerName 
+
+}
+
+function setBestCustomer() {
+
+    bestCustomer = "not bob"
+
+}
+
+function overwriteBestCustomer() {
+
+    bestCustomer = "maybe bob"
+
+}
+
+const leastFavoriteCustomer = "rey"
+
+function changeLeastFavoriteCustomer() {
+
+    leastFavoriteCustomer = "bob"
+
+}
+
+
+
+
